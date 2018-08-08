91251f196f11d7091fc288919a782a72048b8fc7
Geoffrey42 <geoffreygane@gmail.com>
2018-08-08 14:44:00 +0200

    update test/connection_test.js to manage concurrency between tests with before()

--- server/test/connection_test.js
+++ server/test/connection_test.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/rooms_booking', { useNewUrlParser: true });
+before( (done) => {
+    mongoose.connect('mongodb://localhost:27017/rooms_booking', { useNewUrlParser: true });
 
-mongoose.connection
-    .once('open', () =>
-    {
-        console.log('Connection is established')
-    })
-    .on('error', (error) =>
-    {
-        console.warn('Error during connection : ', error)
-    })
+    mongoose.connection
+        .once('open', () =>
+                {
+                    console.log('Connection is established');
+                    done()
+                })
+        .on('error', (error) =>
+                {
+                    console.warn('Error during connection : ', error)
+                })
+})
