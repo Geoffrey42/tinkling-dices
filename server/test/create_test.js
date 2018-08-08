a21688ceaa76fc9865c0d0b64af58ace4ef28fbc
Geoffrey42 <geoffreygane@gmail.com>
2018-08-08 20:37:53 +0200

    fix test/create_test.js minor typo and debug

--- server/test/create_test.js
+++ server/test/create_test.js
@@ -6,14 +6,11 @@ describe('Create test', () => {
     it('Room saving', (done) => {
         rawJSON = fs.readFileSync('./originalDataSet/rooms.json')
         var parsedContent = JSON.parse(rawJSON)
-        console.log(parsedContent.rooms[0])
         for (var i = 0; i < parsedContent.rooms.length; i++) {
             const rooms = new Room(parsedContent.rooms[i])
             rooms
                 .save()
-                .then( () => {
-                    assert(!rooms.isNew);
-            });
+                .then( () => { assert(!rooms.isNew); });
         }
         done();
     })
