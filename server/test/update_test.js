8e5825059c30c25637635d5f93334df5cd33b2e1
Geoffrey42 <geoffreygane@gmail.com>
2018-08-09 00:45:49 +0200

    update test/update_test.js to search and update by date or id

--- server/test/update_test.js
+++ server/test/update_test.js
@@ -5,6 +5,7 @@ var fs = require('fs')
 describe('Update test', () => {
     // create new bookings collection because collection is drop between each file
     let booking_1;
+    let currentEndsAt = 'July 5, 1999'
     let newEndsAt = new Date('July 3, 1999');
 
     beforeEach( (done) => {
@@ -12,7 +13,7 @@ describe('Update test', () => {
             {
                 roomId: "5b6b600a568d1a29d360f069",
                 beginsAt: new Date('July 1, 1999'),
-                endsAt: new Date('July 5, 1999')
+                endsAt: new Date(currentEndsAt)
             });
         booking_1
             .save()
@@ -36,6 +37,14 @@ describe('Update test', () => {
     })
 
     it('Update via model', (done) => {
-        assertEndsAt(Booking.update({endsAt:new Date('July 5, 1999')},{endsAt:newEndsAt}), done)
+        assertEndsAt(Booking.update({endsAt:new Date(currentEndsAt)},{endsAt:newEndsAt}), done)
+    })
+
+    it('Find by date and update', (done) => {
+        assertEndsAt(Booking.findOneAndUpdate({endsAt: new Date(currentEndsAt)}, {endsAt:newEndsAt}),done)
+    })
+
+    it('Find by id and update', (done) => {
+        assertEndsAt(Booking.findByIdAndUpdate(booking_1._id, {endsAt:newEndsAt}), done)
     })
 })
