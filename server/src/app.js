a71f564ef9f9f62a89d91e3f93fbd3a38f962192
Geoffrey42 <geoffreygane@gmail.com>
2018-08-08 04:19:17 +0200

    update src/app.js to connect server to a database called posts

--- server/src/app.js
+++ server/src/app.js
@@ -8,6 +8,13 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/posts');
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function(callback) {
+    console.log("Connection Succeeded");
+});
 app.get('/posts', (req, res) => {
     res.send(
         [{
