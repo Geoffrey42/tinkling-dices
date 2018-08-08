27bb7bd1820afa6f76600cb09f853858d6862486
Geoffrey42 <geoffreygane@gmail.com>
2018-08-08 22:55:29 +0200

    fix test/read_test.js beforeEach() issue by creating fake room instead of parsing json file

--- server/test/read_test.js
+++ server/test/read_test.js
@@ -4,28 +4,46 @@ var fs = require('fs')
 
 describe('Read test', () => {
     // create new rooms collection because collection is drop between each file
-    rawJSON = fs.readFileSync('./originalDataSet/rooms.json')
-    var parsedContent = JSON.parse(rawJSON)
-    const rooms = new Room(parsedContent)
+    let room_1;
 
-    beforeEach((done) => {
-        rooms
+    beforeEach( (done) => {
+        room_1 = new Room(
+            {
+                name: "Salle #1",
+                description: "Salle #1",
+                capacity: 5,
+                equipements: [
+                {
+                    name: "TV"
+                },
+                {
+                    name: "Retro Projecteur"
+                }
+                ],
+                createdAt: "2016-12-07T12:39:29.812Z",
+                updatedAt: "2016-12-08T17:31:39.489Z"
+            });
+        room_1
             .save()
             .then( () => {
-            done();
-        });
+                done();
+            })
     })
-
     it('Find Room by name', (done) => {
         Room
-            //.findOne({'rooms.0.name': 'Salle #1'})
-            .findOne({'rooms.0.name': 'Salle #1'})
+            .find({'name': 'Salle #1'})
             .then( (resultRooms) => {
-                console.log(resultRooms.rooms[0]);
+                assert(resultRooms[0]._id.equals(room_1._id))
                 done();
-        })
+            })
     })
 
-    it('Find Room by id', () => {
+    it('Find Room by id', (done) => {
+        Room
+            .findOne({"_id":room_1._id})
+            .then( (resultRoom) => {
+                assert(resultRoom.name === room_1.name)
+                done();
+            })
     })
 })
