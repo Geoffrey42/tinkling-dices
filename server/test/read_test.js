dd71bc3d6977987ea33a852e6b241f2db5465b18
Geoffrey42 <geoffreygane@gmail.com>
2018-08-08 17:05:23 +0200

    add test/read_test.js to check find query with name

---
+++ server/test/read_test.js
@@ -0,0 +1,26 @@
+const assert = require('assert')
+const Room = require('../models/rooms')
+var fs = require('fs')
+
+describe('Read test', () => {
+    // create new rooms collection because collection is drop between each file
+    rawJSON = fs.readFileSync('./originalDataSet/rooms.json')
+    var parsedContent = JSON.parse(rawJSON)
+    const rooms = new Room(parsedContent)
+
+    beforeEach((done) => {
+        rooms.save().then( () => {
+            done();
+        });
+    })
+
+    it('Find Room by name', (done) => {
+        Room.findOne({'rooms.0.name': 'Salle #1'}).then( (resultRooms) => {
+            console.log(resultRooms.rooms[0]);
+            done();
+        })
+    })
+
+    it('Find Room by id', () => {
+    })
+})
