8be887dcda4fe332bb375cd6fc6321c3df69b8ab
Geoffrey42 <geoffreygane@gmail.com>
2018-08-09 22:01:06 +0200

    refacto App.vue for further component architecture

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,30 @@
 <template>
   <div id="app">
-    <router-link to="/">Go to /</router-link>
-    <router-link to="/posts">Go to /posts</router-link>
-    <router-view></router-view>
+      <app-header></app-header>
+      <app-filter></app-filter>
+      <app-rooms-grid></app-rooms-grid>
+      <app-footer></app-footer>
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue';
+import Filter from './components/Filter.vue';
+import RoomsGrid from './components/RoomsGrid.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   name: 'app',
+  components: {
+      'app-header': Header,
+      'app-filter': Filter,
+      'app-rooms-grid': RoomsGrid,
+      'app-footer': Footer
+  }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
