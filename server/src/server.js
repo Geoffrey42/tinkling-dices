0a5955580948d12ebfb519049ef4e1e716467b35
Geoffrey42 <geoffreygane@gmail.com>
2018-08-09 14:25:44 +0200

    update src/server.js to populate db at server start

--- server/src/server.js
+++ server/src/server.js
@@ -13,12 +13,28 @@ server.use(bodyParser.json())
 server.use(cors())
 routes(server);
 
-var dbName = 'sf_booking';
+let dbName = 'sf_booking';
 mongoose.connect('mongodb://localhost:27017/' + dbName, { useNewUrlParser: true });
 
-var db = mongoose.connection;
+let db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection to " + dbName + " failed"));
 db.once("open", () => console.log("Connection to " + dbName + " Succeeded"));
+
+Room.findOne({'name':'Salle #1'})
+    .then( (result) => {
+        if (result) {
+            console.log(dbName + ' is populated')
+        } else {
+            console.log('populating ' + dbName);
+            const rawJSON = fs.readFileSync('./originalDataSet/rooms.json');
+            const  parsedContent = JSON.parse(rawJSON);
+            for (let i = 0; i < parsedContent.rooms.length; i++) {
+                const room = new Room(parsedContent.rooms[i]);
+                room.save();
+            }
+            console.log(dbName + ' is now populated');
+        }
+    })
 
 
 server.listen(process.env.PORT || 8081)
