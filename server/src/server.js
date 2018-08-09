12ff7906c40c4bba61e8ca4a10981210676ee31a
Geoffrey42 <geoffreygane@gmail.com>
2018-08-09 09:13:56 +0200

    update src/server.js to better db connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,23 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 const routes = require('../routes/index')
-
 const server = express()
-routes(server);
+const Room = require("../models/rooms");
+const fs = require('fs');
+
 server.use(morgan('combined'))
 server.use(bodyParser.json())
 server.use(cors())
+routes(server);
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/posts', { useNewUrlParser: true });
+var dbName = 'sf_booking';
+mongoose.connect('mongodb://localhost:27017/' + dbName, { useNewUrlParser: true });
+
 var db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error"));
-db.once("open", function(callback) {
-    console.log("Connection Succeeded");
-});
+db.on("error", console.error.bind(console, "Connection to " + dbName + " failed"));
+db.once("open", () => console.log("Connection to " + dbName + " Succeeded"));
 
-var rooms = require("../models/rooms");
 
 server.listen(process.env.PORT || 8081)
