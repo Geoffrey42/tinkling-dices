9e4171fc281cdeb3ea4eaebe049f25645df43559
Geoffrey42 <geoffreygane@gmail.com>
2018-08-10 00:43:41 +0200

    refacto src/router/index.js to remove useless routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,21 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HelloWorld from '../components/HelloWorld'
-import Posts from '../components/Posts'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
     routes: [
-        {
-            path: '/',
-            name: 'helloWorld',
-            component: HelloWorld
-        },
-        {
-            path: '/posts',
-            name: 'posts',
-            component: Posts
-        }
     ]
 })
