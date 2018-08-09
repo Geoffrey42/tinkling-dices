9cbfd2c201610f5e2822090b8d545142259c2d8c
Geoffrey42 <geoffreygane@gmail.com>
2018-08-09 09:12:11 +0200

    update controllers/room-controller.js to handle post requests

--- server/controllers/room-controller.js
+++ server/controllers/room-controller.js
@@ -1,8 +1,14 @@
 module.exports = {
-    getRooms(req, res) {
+    readAll(req, res) {
         res.send({rooms: 'des rooms'});
     },
-    getRoom(req, res) {
+    read(req, res) {
         res.send({room: 'une room avec id ' + req.params.id});
+    },
+    create(req, res) {
+        const body = req.body;
+        console.log(req);
+        console.log(JSON.stringify(body));
+        console.log(body);
     }
 }
