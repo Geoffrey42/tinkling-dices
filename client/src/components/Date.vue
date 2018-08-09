d4d33c8d7e19e37599c4387a3942fab7a186cd42
Geoffrey42 <geoffreygane@gmail.com>
2018-08-10 00:42:42 +0200

    update src/components/Date.vue to implement buefy date picker component

---
+++ client/src/components/Date.vue
@@ -0,0 +1,10 @@
+<template>
+    <b-field label="Select a date">
+        <b-datepicker
+             placeholder="Click to select..."
+             icon="calendar-today">
+        </b-datepicker>
+    </b-field>
+</template>
+<script>
+</script>
