1ad94629d93b5d9f6f1ade41d92d31ec771b6ef1
Geoffrey42 <geoffreygane@gmail.com>
2018-08-09 14:21:52 +0200

    update controllers/room-controller.js to manage get requests on all or one specific room (by id)

--- server/controllers/room-controller.js
+++ server/controllers/room-controller.js
@@ -1,14 +1,18 @@
+const Room = require('../models/rooms')
+
 module.exports = {
     readAll(req, res) {
-        res.send({rooms: 'des rooms'});
+        Room
+            .find()
+            .then( (results) => {
+                res.send(results);
+            })
     },
     read(req, res) {
-        res.send({room: 'une room avec id ' + req.params.id});
-    },
-    create(req, res) {
-        const body = req.body;
-        console.log(req);
-        console.log(JSON.stringify(body));
-        console.log(body);
+        Room
+            .findOne({'_id': req.params.id})
+            .then( (result) => {
+                res.send(result);
+            })
     }
 }
