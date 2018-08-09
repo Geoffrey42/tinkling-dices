5e2c705fe67e4c3f107f563348c9b14b37e108a6
Geoffrey42 <geoffreygane@gmail.com>
2018-08-09 14:24:48 +0200

    update routes/index.js to remove obsolete POST request on rooms model

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 RoomController = require('../controllers/room-controller');
+BookingController = require('../controllers/booking-controller');
+
 module.exports = (server) => {
     server.get('/rooms', RoomController.readAll);
     server.get('/rooms/:id', RoomController.read);
-    server.post('/rooms', RoomController.create);
+    server.post('/booking', BookingController.create);
 }
