85322218a3a8be27ee6b5bbbef76f466e605001a
Geoffrey42 <geoffreygane@gmail.com>
2018-08-09 14:56:06 +0200

    update routes/index.js to create two GET routes for booking model

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,8 @@ BookingController = require('../controllers/booking-controller');
 module.exports = (server) => {
     server.get('/rooms', RoomController.readAll);
     server.get('/rooms/:id', RoomController.read);
+
+    server.get('/bookings', BookingController.readAll);
+    server.get('/bookings/:id', BookingController.read);
     server.post('/booking', BookingController.create);
 }
