ee3ad94d0e3e3d3a246b18ff4e2dca1795fb7ef6
Geoffrey42 <geoffreygane@gmail.com>
2018-08-13 19:34:56 +0200

    add Form.vue to use a form instead of a grid for filters

---
+++ client/src/components/Form.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <form>
+            <form-date></form-date>
+            <form-hour></form-hour>
+            <form-capacity></form-capacity>
+            <form-equipements></form-equipements>
+        </form>
+    </div>
+</template>
+
+<script>
+import MyDate from './Date.vue';
+import Hour from './Hour.vue';
+import Capacity from './Capacity.vue';
+import Equipements from './Equipements.vue';
+
+export default {
+    components: {
+        'form-date': MyDate,
+        'form-hour': Hour,
+        'form-capacity': Capacity,
+        'form-equipements': Equipements
+    }
+}
+
+</script>
