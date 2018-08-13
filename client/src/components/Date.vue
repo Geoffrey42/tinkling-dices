4be381a49579b6572d14ee117fd9c88287f8cd43
Geoffrey42 <geoffreygane@gmail.com>
2018-08-13 23:05:24 +0200

    update components/Date.vue to send selected date to parent form component

--- client/src/components/Date.vue
+++ client/src/components/Date.vue
@@ -2,9 +2,18 @@
     <b-field>
         <b-datepicker
              placeholder="Select a date"
-             icon="calendar-today">
+             icon="calendar-today"
+             @input="sendDate($event)">
         </b-datepicker>
     </b-field>
 </template>
 <script>
+
+export default {
+    methods: {
+        sendDate(event) {
+            this.$emit('dateWasPicked', event);
+        }
+    }
+}
 </script>
