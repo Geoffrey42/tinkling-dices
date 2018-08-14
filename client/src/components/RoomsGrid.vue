99f8fdcf487a1bdab663bfdf6127bdfff08fdb7a
Geoffrey42 <geoffreygane@gmail.com>
2018-08-14 05:06:40 +0200

    update src/components/RoomsGrid.vue to perform get request on rooms

--- client/src/components/RoomsGrid.vue
+++ client/src/components/RoomsGrid.vue
@@ -1,11 +1,37 @@
 <template>
-    <div>
+    <div class="columns">
+        <rooms-grid-room
+            class="column"
+            v-for="room in rooms"
+            :key="room._id"
+            :roomName="room.name"></rooms-grid-room>
     </div>
 </template>
 
 <script>
+import RoomService from '../services/RoomService'
+import Room from './Room.vue';
 
+/* eslint-disable */
 export default {
+    props: ['userInput'],
+    data() {
+        return {
+            rooms: []
+        }
+    },
+    components: {
+        'rooms-grid-room': Room
+    },
+    mounted() {
+        RoomService.getAllRooms()
+            .then( (fetchedRooms) => {
+                for (var i = 0; i < fetchedRooms.data.length; i++) {
+                    this.rooms.push(fetchedRooms.data[i])
+                }
+            })
+            .catch( (error) => console.log(error))
+    }
 }
 
 </script>
