11fa9ac7e50becb79026efd56222e59c3d560dd0
Geoffrey42 <geoffreygane@gmail.com>
2018-08-14 09:07:18 +0200

    debug client/src/components/Room.vue to find server issue origin

--- client/src/components/Room.vue
+++ client/src/components/Room.vue
@@ -7,16 +7,22 @@
 <script>
 import BookingService from '../services/BookingService'
 
+
 export default {
     props: ['roomName', 'roomId', 'userInput'],
     methods: {
         sendBooking() {
+            console.log(this.userInput.date);
             var rawData = {
                 roomId: this.roomId,
                 date: this.userInput.date,
                 hour: this.userInput.hour
             };
+            console.log("in Vue.js (rawData):");
+            console.log(rawData);
             var jsonBooking = JSON.stringify(rawData);
+            console.log("in Vue.js (json):");
+            console.log(jsonBooking);
             BookingService.postBooking(jsonBooking)
         }
     }
